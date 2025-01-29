f085be62f4814e819a0daf32f6a58f678ec9d372
ksmfou98 <ksmfou98@naver.com>
2025-01-29 23:11:35 +0900

    feat vanilla extract, mantine 적용

--- src/app/compress-image/page.tsx
+++ src/app/compress-image/page.tsx
@@ -1,5 +1,14 @@
+import * as style from "./page.css";
+import { Button, Title } from "@mantine/core";
+
 function Page() {
-  return <div>compress asd</div>;
+  return (
+    <div className={style.container}>
+      compress asd
+      <Title>이미지 압축</Title>
+      <Button>버튼</Button>
+    </div>
+  );
 }
 
 export default Page;
